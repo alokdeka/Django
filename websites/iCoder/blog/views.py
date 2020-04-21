00bdb4e8d087b0bb6e50d40a2c493f520d8e1749
alok <work.deka.alok@gmail.com>
2020-04-22 03:07:03 +0530

    users can post comment: added

--- websites/iCoder/blog/views.py
+++ websites/iCoder/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
-from .models import Post
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Post, BlogComment
+from django.contrib import messages
 
 # Create your views here.
 
@@ -14,7 +15,22 @@ def blogHome(request):
 
 def blogPost(request, slug):
     post = Post.objects.filter(slug=slug).first()
-    context = {'post': post}
+    comments = BlogComment.objects.filter(post=post)
+    context = {'post': post, 'comments': comments}
     # print(post)
     return render(request, 'blog/blogPost.html', context)
     # return HttpResponse(f"This is Blog: {slug}")
+
+
+def postComment(request):
+    if request.method == 'POST':
+        comment = request.POST.get('comment')
+        user = request.user
+        postSno = request.POST.get('postSno')
+        post = Post.objects.get(sno=postSno)
+
+        comment = BlogComment(comment=comment, user=user, post=post)
+        comment.save()
+        messages.success(
+            request, 'Your comment has been posted successfully !')
+    return redirect(f'/blog/{post.slug}')
